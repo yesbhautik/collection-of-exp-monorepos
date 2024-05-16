3fb8672e2ef6cf1488edf6d7e2321ca972400bb3
Bhautik Bavadiya <hi@yesbhautik.co.in>
2024-05-16 17:33:01 +0530

    right updated

--- kutta-xpair/index.js
+++ kutta-xpair/index.js
@@ -15,7 +15,7 @@ res.sendFile(__path + '/main.html')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-    console.log(`YoutTube: @DGXeon\nTelegram: xeonbotinc\nGitHub: @DGXeon\nInstsgram: unicorn_xeon13\n\nServer running on http://localhost:` + PORT)
+    console.log(`YoutTube: @yesbhautik\nTelegram: @yesbhautik\nGitHub: @yesbhautik\nInstsgram: @yesbhautik\n\nServer running on http://localhost:` + PORT)
 })
 
 module.exports = app
