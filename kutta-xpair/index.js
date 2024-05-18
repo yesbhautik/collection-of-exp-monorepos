57754353fb69e1679bd85d3340a4e5e308a84e72
Yashvi shah <yashu.shah28@gmail.com>
2024-05-18 15:05:26 +0530

    Revert "Update index.js"
    This reverts commit 60ef33a119479fd86270f9fddded966ff466c15a.

--- kutta-xpair/index.js
+++ kutta-xpair/index.js
@@ -4,15 +4,14 @@ __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let code = require('./pair');
-const path = require('path');
 require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/code', code);
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(path.join(__dirname, '/pair.html'));
-});
-app.use('/', async (req, res, next) => {
-    res.sendFile(path.join(__dirname, '/main.html'));
-});
+app.use('/pair',async (req, res, next) => {
+res.sendFile(__path + '/pair.html')
+})
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/main.html')
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
